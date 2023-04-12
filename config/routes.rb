1db8237c109742c9bb23a7625c788dd743d894ea
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-12 11:31:09 +0900

    ルーティングの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
+  resources :blogs do
+    collection do
+      post :confirm
+    end
+  end
+  resources :favorites, only: [:create, :destroy]
+  # メールの時必要？
+  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
