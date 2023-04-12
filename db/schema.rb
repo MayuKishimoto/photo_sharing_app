09e61bc9e736590b09a5a4fcf11b4ce40f41618f
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-12 16:07:43 +0900

    usersテーブルのカラム名変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_12_051646) do
+ActiveRecord::Schema.define(version: 2023_04_12_065223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2023_04_12_051646) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.text "image"
+    t.text "user_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
