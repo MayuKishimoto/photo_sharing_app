45b0e2e57da1602555d6bca2d21fa5a86e5647d0
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-17 11:02:28 +0900

    pictureコントローラ作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  # before_action :set_picture, only: %i[ show edit update destroy ]
+  before_action :set_picture, only: %i[ show edit update destroy ]
 
   def index
     @pictures = Picture.all
@@ -10,16 +10,16 @@ class PicturesController < ApplicationController
   end
 
   def create
-    # @picture = current_user.pictures.build(picture_params)
-    # if params[:back]
-    #   render :new
-    # else
+    @picture = current_user.pictures.build(picture_params)
+    if params[:back]
+      render :new
+    else
       if @picture.save
         redirect_to pictures_path, notice: "投稿を作成しました！"
       else
         render :new
       end
-    # end
+    end
   end
 
   def show
@@ -27,10 +27,33 @@ class PicturesController < ApplicationController
     # @favorite = current_user.favorites.find_by(picture_id: @picture.id)
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @picture = current_user.pictures.build(picture_params)
+    render :new if @picture.invalid?
+  end
+
   private
 
   def picture_params
     params.require(:picture).permit(:image, :content, :image_cache)
   end
-
 end
