3cc90b37764bc58a047c91749453f46679ffe851
mayu_kishimoto <nhpw.29@gmail.com>
2023-04-17 14:45:05 +0900

    view下準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'sessions#new'
+  root 'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :pictures do
